44280297caec65393a3e3284d92d7216d3b6ea15
dmitry <rbdeman@yandex.ru>
2023-06-06 22:36:55 +0300

    добавил функцию редактирования заметки

--- main.py
+++ main.py
@@ -29,20 +29,39 @@ def dell(list_notes):
         print("заметка успешно удалена!")
     else:
         print("заметка не найдена!")
-    
 
+def change(list_notes):
+    count = 0
+    num = int(input("введи ID(номер) заметки для редактирования: "))
+    for k in list_notes:
+        if (k == num):
+            title = input("введи заголовок заметки: ")
+            text = input("введи текст заметки: ")
+            dt_now = datetime.datetime.now()
+            assembling = []
+            assembling.append(title)
+            assembling.append(text)
+            assembling.append(dt_now)
+            list_notes[k] = assembling
+            count = count+1
+    if (count >= 1):
+        print("заметка успещно изменена!")
+    else:
+        print("заметка с данным ID не найдена!")
 
 
 
 while True:
     
     command = input("введи команду: ")
-    if(command == "add"):
+    if (command == "add"):
         add (notes_list, next_key)
         next_key = next_key + 1
         print ("заметка успешно добавлена!")
-    elif(command == "show"):
+    elif (command == "show"):
         show(notes_list)
     elif (command == "dell"):
         dell(notes_list)
+    elif (command == "change"):
+        change(notes_list)
         
