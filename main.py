efa2317f529d9745476ae8cdbd4ea37d85d222ef
dmitry <rbdeman@yandex.ru>
2023-06-06 22:16:05 +0300

    добавил функцию удаления

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ next_key = 1
 
 
 
-
 def add(list_notes, key_next):
     title = input("введи заголовок заметки: ")
     text = input("введи текст заметки: ")
@@ -18,9 +17,18 @@ def add(list_notes, key_next):
     
 def show(list_notes):
     print("список заметок: ", list_notes)
-    
 
-
+def dell(list_notes):
+    count = 0
+    num = int(input("введи ID(номер) заметки: "))
+    for k in list_notes.copy():
+        if (k == num):
+            count = count+1
+            del list_notes[k]
+    if (count >= 1):
+        print("заметка успешно удалена!")
+    else:
+        print("заметка не найдена!")
     
 
 
@@ -29,10 +37,12 @@ def show(list_notes):
 while True:
     
     command = input("введи команду: ")
-    if (command == "add"):
+    if(command == "add"):
         add (notes_list, next_key)
-        next_key = next_key +1
+        next_key = next_key + 1
         print ("заметка успешно добавлена!")
-    elif (command == "show"):
+    elif(command == "show"):
         show(notes_list)
+    elif (command == "dell"):
+        dell(notes_list)
         
